cdb210e2381ab0a6e661b1c173cb2d96594229d6
Chris Gonzales <cgonzo86@gmail.com>
2011-04-04 15:40:12 -0700

    add hadoop.sh to run mapreduce job

---
+++ hadoop.sh
@@ -0,0 +1,16 @@
+export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
+hadoop fs -copyFromLocal scowiki-20090929-one-page-per-line scowiki-20090929-one-page-per-line
+
+hadoop fs -rmr linkmap0
+hadoop jar $SJAR -mapper "$(pwd)/linkmap.py map" -reducer "$(pwd)/linkmap.py reduce" -input scowiki-20090929-one-page-per-line -output linkmap0
+for i in {0..9}
+do
+input_linkmap="linkmap$i"
+output_linkmap="linkmap$((i+1))"
+reducer_output="results$i"
+hadoop fs -rmr $output_linkmap
+hadoop jar $SJAR -mapper "$(pwd)/pagerank.py map" -reducer "$(pwd)/pagerank.py reduce" -input $input_linkmap -output $output_linkmap
+hadoop fs -rmr $reducer_output
+hadoop jar $SJAR -mapper "$(pwd)/pr_out.py map" -reducer "sort" -input $output_linkmap -output $reducer_output
+done
+
