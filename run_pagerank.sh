0cf208a17a13bbe9e629061d3df07df04468247f
Chris Gonzales <cgonzo86@gmail.com>
2011-04-05 08:51:00 -0700

    fix pagerank.sh to use other data sources

--- run_pagerank.sh
+++ run_pagerank.sh
@@ -1,7 +1,7 @@
 # run_pagerank.sh
 # ./run_pagerank.sh data num_reducers
 export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
-hadoop fs -copyFromLocal scowiki-20090929-one-page-per-line scowiki-20090929-one-page-per-line
+hadoop fs -copyFromLocal $1 $1
 
 hadoop fs -rmr linkmap0
 hadoop jar $SJAR -mapper "$(pwd)/linkmap.py map" -reducer "$(pwd)/linkmap.py reduce" -input $1 -output linkmap0
