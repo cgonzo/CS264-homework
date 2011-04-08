3f8c041f6b9f97f1fe7dcd7bd25180e8108fdb23
Chris Gonzales <cgonzo86@gmail.com>
2011-04-08 13:54:53 -0400

    should work on ec2 now

--- run_pagerank.sh
+++ run_pagerank.sh
@@ -4,7 +4,7 @@ export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
 hadoop fs -copyFromLocal $1 $1
 
 hadoop fs -rmr linkmap0
-hadoop jar $SJAR -mapper "$(pwd)/linkmap.py map" -reducer "$(pwd)/linkmap.py reduce" -input $1 -output linkmap0
+hadoop jar $SJAR -mapper "/usr/bin/python linkmap.py map" -reducer "/usr/bin/python linkmap.py reduce" -input $1 -output linkmap0 -file "files/linkmap.py" -file "files/common.py"
 for i in {0..9}
 do
 input_linkmap="linkmap$i"
@@ -14,12 +14,12 @@ hadoop fs -rmr $output_linkmap
 if [ $2 ]
 then
 echo "Using $2 reducers"
-hadoop jar $SJAR -mapper "$(pwd)/pagerank.py map" -reducer "$(pwd)/pagerank.py reduce" -input $input_linkmap -output $output_linkmap -numReduceTasks $2
+hadoop jar $SJAR -mapper "/usr/bin/python pagerank.py map" -reducer "/usr/bin/python pagerank.py reduce" -input $input_linkmap -output $output_linkmap -numReduceTasks $2 -file "files/pagerank.py" -file "files/common.py"
 else
 echo "Using default reducers"
-hadoop jar $SJAR -mapper "$(pwd)/pagerank.py map" -reducer "$(pwd)/pagerank.py reduce" -input $input_linkmap -output $output_linkmap
+hadoop jar $SJAR -mapper "/usr/bin/python pagerank.py map" -reducer "/usr/bin/python pagerank.py reduce" -input $input_linkmap -output $output_linkmap -file "files/pagerank.py" -file "files/common.py"
 fi
 hadoop fs -rmr $reducer_output
-hadoop jar $SJAR -mapper "$(pwd)/pr_out.py map" -reducer "sort" -input $output_linkmap -output $reducer_output
+hadoop jar $SJAR -mapper "/usr/bin/python pr_out.py map" -reducer "/usr/bin/sort" -input $output_linkmap -output $reducer_output -file "files/pr_out.py" -file "files/common.py"
 done
 
