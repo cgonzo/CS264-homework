174cec6984572ba60392aa5792256e84b90723ee
Chris Gonzales <cgonzo86@gmail.com>
2011-04-08 17:41:17 -0400

    re-write reduction

--- files/wordcount.py
+++ files/wordcount.py
@@ -2,8 +2,7 @@
 
 import common
 import pyopencl
-from pyopencl.scan import InclusiveScanKern
-import pyopencl.array as cl_array
+import numpy
 
 def map(line):
 	common.ununicode(line)
@@ -11,14 +10,39 @@ def map(line):
 		yield(1,1)
 
 def reduce(word, counts):
-	ctx = pyopencl.create_some_context()
-	queue = cl.CommandQueue(ctx)
-	knl = InclusiveScanKernel(ctx, np.int32, "a+b")
-	counts_gpu = cl_array.to_device(queue,counts)
+	counts_int=counts.astype(numpy.int)
+	counts_gpu = cuda.mem_alloc(counts.nbytes)
+	cuda.memcpy_htod(counts_gpu, counts)
 	
-	knl(counts_gpu)
-	counts_cpu=counts_gpu.get
-	yield(1,counts_cpu[len(counts_cpu)-1])
+	mod = SourceModule("""
+__global__ void reduction(float *g_data, int n)
+{
+	int index=blockIdx.x*blockDim.x+threadIdx.x;
+    // Do sum reduction from shared memory
+	int numberOfCalculationsForThisStep=(blockDim.x+1)/2;
+	while(numberOfCalculationsForThisStep>0)
+	{
+		if(threadIdx.x<numberOfCalculationsForThisStep)
+		{
+			g_data[index]=g_data[index]+g_data[index+numberOfCalculationsForThisStep];
+		}
+		numberOfCalculationsForThisStep/=2;
+		__syncthreads();
+	}
+    // Store result from shared memory  back to global memory
+	if(threadIdx.x==0)
+	{
+		g_data[blockIdx.x]=s_data[threadIdx.x];
+	}
+    return;
+}
+  """)
+	
+	func = mod.get_function("reduction")
+	func(counts_gpu)
+	counts_return = numpy.empty_like(counts)
+	cuda.memcpy_dtoh(counts_return, counts)
+	yield(1,counts_return[0])
 
 if __name__ == "__main__":
   common.main(map, reduce)
