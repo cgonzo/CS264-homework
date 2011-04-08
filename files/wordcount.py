2f27d9f80604473f5e77572285b1e84618ba4100
Chris Gonzales <cgonzo86@gmail.com>
2011-04-08 18:11:51 -0400

    minor

--- files/wordcount.py
+++ files/wordcount.py
@@ -27,8 +27,8 @@ __global__ void reduction(float *g_data)
 	
 	func = mod.get_function("reduction")
 	func(counts_gpu,block=(1,1,1))
-	counts_return = numpy.empty_like(counts)
-	cuda.memcpy_dtoh(counts_return, counts)
+	counts_return = numpy.empty_like(counts_gpu)
+	cuda.memcpy_dtoh(counts_return, counts_gpu)
 	yield("1",counts_return[0])
 
 if __name__ == "__main__":
