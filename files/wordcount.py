cb3803207c17ab8ed227e8c4b2c4dc328076f5ee
Chris Gonzales <cgonzo86@gmail.com>
2011-04-09 15:21:41 -0400

    more changes

--- files/wordcount.py
+++ files/wordcount.py
@@ -46,8 +46,9 @@ __global__ void reduction(float *g_data,int n)
 	threadsPerBlock=512;
 	numBlocks=math.ceil((totalsize+threadsPerBlock-1)/threadsPerBlock)
 	print "totalsize:"+str(totalsize)+" numBlocks:"+str(numBlocks)
+	numelements=totalsize
 	while(numBlocks>0):
-		func(counts_gpu,numpy.int32(totalsize),block=(threadsPerBlock,1,1),grid=(int(numBlocks),1))
+		func(counts_gpu,numpy.int32(numelements),block=(threadsPerBlock,1,1),grid=(int(numBlocks),1))
 		numelements=numBlocks
 		if(numBlocks==1):
 			numBlocks=0
