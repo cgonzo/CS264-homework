321b718c9c370dfb4bc88dd47f4627857312fcfa
Chris Gonzales <cgonzo86@gmail.com>
2011-04-09 15:44:51 -0400

    Almost done; now to test on hadoop

--- run_gpu_wordcount.sh
+++ run_gpu_wordcount.sh
@@ -3,5 +3,4 @@
 export SJAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-CDH3B4.jar
 hadoop fs -copyFromLocal $1 $1
 
-hadoop fs -rmr $(1)_wc
-hadoop jar $SJAR -mapper "/usr/bin/python2.6 wordcount.py map" -reducer "/usr/bin/python2.6 wordcount.py reduce" -input $1 -output linkmap0 -file "files/wordcount.py" -file "files/common.py"
+hadoop jar $SJAR -mapper "/usr/bin/python2.6 wordcount.py map" -reducer "/usr/bin/python2.6 wordcount.py reduce" -input $1 -output linkmap0 -file "files/wordcount.py" -file "files/common.py" -cmdenv PATH=$PATH
